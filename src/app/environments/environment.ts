7fa860f0bd974aa064aa5dd245489240bebaefff
Mateus Melo <mateus.gon.melo@gmail.com>
2026-09-10 18:30:17 -0300

    deploy vercell

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyBp8LPQntU4UD1kpQbnKJ9jnE5B_JKuKFA',
+    authDomain: 'angularestoque.firebaseapp.com',
+    projectId: 'angularestoque',
+    storageBucket: 'angularestoque.appspot.com',
+    messagingSenderId: '634571606343',
+    appId: '1:634571606343:web:58bbd96dccfa2e2c2a6462',
+  },
+};
